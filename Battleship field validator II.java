baee6df5316d91562a8c93cc7d53f7aad6022521
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2024-12-01 04:26:54 -0500

    Create Battleship field validator II.java
    This is technically my second attempt at the battleship field validator after having never succeeded at solving the first one without timing out.

---
+++ Battleship field validator II.java
@@ -0,0 +1,15 @@
+/**
+ * https://www.codewars.com/kata/571ec81d7e8954ce1400014f/java
+ */
+
+public class BF {
+
+  public BF(int[][] field) {
+    // your code here!
+  }
+
+  public boolean validate() {
+    // your code here too!
+    return false;
+  }
+}
