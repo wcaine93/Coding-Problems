7bdda088046aa50f6d9d4e15e91a61f6902131d7
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2024-12-13 10:57:48 -0500

    Update Assembler interpreter (part II).java
    Removed command codes as switch in java 17+ can match against Strings, began implementation of command recognition

--- Assembler interpreter (part II).java
+++ Assembler interpreter (part II).java
@@ -14,38 +14,14 @@ public class AssemblerInterpreter {
   static Map<String, Integer> registers = new HashMap<>();
   static Map<String, Integer> labels = new HashMap<>(); // links register names to line numbers
   
-  // assign a code to each command
-  static final Map<String, Integer> commands = Map.ofEntries(
-    // x is the first input, y is the second input
-    entry("mov", 12), // copies x into register y
-    entry("inc", 11), // register x++
-    entry("dec", 21), // register x--
-    entry("add", 22), // register x += y
-    entry("sub", 32), // register x -= y
-    entry("mul", 42), // register x *= y
-    entry("div", 52), // register x /= y
-    entry(":", 10), // label
-    entry("jmp", 31), // jumps to label x
-    entry("cmp", 62), // compares x and y (see cmp codes below)
-    entry("jne", 0), // jumps to x if cmp value is 0
-    entry("je", 1), // jumps to x if cmp value is 1
-    entry("jge", 2), // jumps to x if cmp value is 2
-    entry("jg", 3), // jumps to x if cmp value is 3
-    entry("jle", 4), // jumps to x if cmp value is 4
-    entry("jl", 5), // jumps to x if cmp value is 5
-    entry("call", 41), // calls subroutine x
-    entry("ret", 20), // returns back to the call
-    entry("msg", 100), // output to stdout
-    entry("end", 30), // terminates execution
-    entry(";", 40) // begin a line comment
-  );
-  
   static ArrayList<String> stdout = new ArrayList<>();
+  static boolean executing = true;
     
   public static String interpret(final String input) {
     label(input);
     
     interpret(input, 0);
+    if (executing) return null; // return null if execution ends without end statement
     
     String output = "";
     for (String s : stdout.toArray(new String[0])) output += s;
@@ -69,9 +45,29 @@ public class AssemblerInterpreter {
   }
   
   public static void interpret(final String input, int callPos) {
+    if (!executing) return;
+    
     Scanner rawCode = new Scanner(input);
+    for (int i = 0; i < callPos; i++) rawCode.nextLine(); // start on line callPos
     
-    
+    while (rawCode.hasNext()) {
+      String read = rawCode.next();
+      switch (read) {
+          case ";" -> break;
+          case "end" -> {
+            executing = false;
+            return;
+          }
+          case "ret" -> return;
+          case "msg" -> {
+            // single quote output
+            if rawCode.hasNext("'\\.\\*"); // /'.*/
+          }
+          default -> throw new Exception("Invalid command");
+      }
+      
+      rawCode.nextLine();
+    }
   }
   
   public static void terminate() {
