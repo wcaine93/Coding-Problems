8191ff879ab5ff566c3d569f36bae335e2537bf0
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2024-12-13 00:38:52 -0500

    Create Assembler interpreter (part II).java

---
+++ Assembler interpreter (part II).java
@@ -0,0 +1,12 @@
+/* 
+ * https://www.codewars.com/kata/58e61f3d8ff24f774400002c
+ *
+ * Accepts a string of assembly language as input and returns the output. See kata for more details.
+ */
+
+public class AssemblerInterpreter {
+  public static String interpret(final String input) {
+    // Your code here!
+    return null;
+  }
+}
