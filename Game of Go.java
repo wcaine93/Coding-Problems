acdad92658862d9b8b01e8e8abf3eac6cf578009
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2025-04-02 21:30:48 -0400

    Implement creation of go board

--- Game of Go.java
+++ Game of Go.java
@@ -4,6 +4,39 @@
  * Implementation of the game of go. See kata for details
  */
 
-public class Go {
+import java.util.Map;
+import java.util.HashMap;
 
+public class Go {
+  private Map<String, Integer> size = new HashMap<>();
+  private int height;
+  private int width;
+  private char[][] board;
+  
+  Go(int size) { // create square board
+    this.size.put("height", size);
+    this.size.put("width", size);
+    
+    createBoard();
+  }
+  Go(int height, int width) {
+    size.put("height", height);
+    size.put("width", width);
+  }
+  
+  public char[][] getBoard() { return this.board; }
+  
+  private void createBoard() throws IllegalArgumentException {
+    if (height <= 0) throw new IllegalArgumentException("Board height must be positive");
+    if (width <= 0) throw new IllegalArgumentException("Board width must be positive");
+    
+    // create empty board of size height x width
+    board = new char[height][width];
+    for (int row = 0; row < height; row++) {
+      for (int col = 0; col < width; col++) {
+        // . represents empty spaces
+        board[row][col] = '.';
+      }
+    }
+  }
 }
