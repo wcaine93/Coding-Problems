c5dfbd191d9baf737f9a678df07a3ef49cfa193e
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2025-04-29 14:55:20 -0400

    Create Decode the Morse code, for real.java

---
+++ Decode the Morse code, for real.java
@@ -0,0 +1,29 @@
+/*
+ * https://www.codewars.com/kata/54acd76f7207c6a2880012bb
+ *
+ * decode Morse Code from 'real-life' scenarios (varying sampling rates and blip/space lengths)
+ */
+
+public class MorseCodeDecoder {
+    
+    /**
+     * Given a string in Morse Code, returns the English translation.
+     *
+     * Accept dots, dashes and spaces, returns human-readable message.
+     */
+    public static String decodeMorse(String morseCode) {
+        //TODO
+    }
+    
+    /**
+     * Given a string of bits, which may or may not begin or end with '0's,
+     * and which may have some variation in the length of the time unit used,
+     * returns the Morse Code translation of this message.
+     *
+     * Accepts 0s and 1s, return dots, dashes and spaces
+     *
+     */
+    public static String decodeBitsAdvanced(String bits) {
+        //TODO
+    }
+}
