54623b3fa766f03906ce094f21bc0cbc81fd5126
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2025-12-12 00:06:53 -0500

    Create Challenge Fun #10: Integer Square Root.java

---
+++ Challenge Fun #10: Integer Square Root.java
@@ -0,0 +1,13 @@
+/*
+ * https://www.codewars.com/kata/58a3fa665973c2a6e80000c4
+ *
+ * Given integer N in [0, 10^100], find integer M such that M^2 < N^2 and (M+1)^2 > N^2.
+ */
+
+public class Kata {
+
+	public static String integerSquareRoot(String n) {
+    
+		return "0";
+	}
+}
